653f2fcbf8af3e839c37abce96e2f014b3b1ac78
suganyara <121349676+suganyara@users.noreply.github.com>
2022-12-25 10:10:36 +0530

    Add files via upload

---
+++ day1/ClassStudent.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class ClassStudent {
+	public void studentRecord() {
+		String studentName ="suganya";
+		int rollNo=2;
+		String collegeName="Kvct";
+		int markScored=96;
+		float cgpa= 7.8f;
+		System.out.println(studentName);
+		System.out.println(rollNo);
+		System.out.println(collegeName);
+		System.out.println(markScored);
+		System.out.println(cgpa);
+	}
+
+}
--- day1/Mobile.java
+++ day1/Mobile.java
@@ -1,15 +1,24 @@
-package weekone.day1;
+package week1.day1;
 
 public class Mobile {
-public void sendsms() {
-	System.out.println("suganya");
+
+public void makeCall() {
+	String mobileModel ="Samsung";
+    String mobileWeight ="356.82f";
+    System.out.println(mobileModel);
+    System.out.println(mobileWeight);
+	
 }
-public void takesnapshot(){
-	System.out.println("image");
+public void sendMsg() {
+	boolean isFullCharged = false;
+	int mobileCost = 25000;
+	System.out.println(isFullCharged);
+	System.out.println(mobileCost);
 }
-public static void main(String[] args) {
-	Mobile mb=new Mobile();
-	mb.sendsms();
-	mb.takesnapshot();
-}
+	public static void main(String[] args) {
+		Mobile mb = new Mobile();
+		mb.makeCall();
+		mb.sendMsg();
+	System.out.println("This is my Mobile");
+	}
 }
